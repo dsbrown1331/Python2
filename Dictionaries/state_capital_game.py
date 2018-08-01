57d4f360d7df2a3868a34321f06f626975906f01
dsbrown1331 <dsbrown1331@gmail.com>
2018-08-01 13:30:24 -0500

    update

--- Dictionaries/state_capital_game.py
+++ Dictionaries/state_capital_game.py
@@ -7,15 +7,21 @@ def get_state_capitals(filename):
         #debug print line
         print("The capital of {} is {}".format(state, capital))
     #TODO: return a dictionary mapping from states to capitals
+    #return put_your_dict_here
 
-
-get_state_capitals("../Data/state_capitals.csv")
+state_cap_dict = get_state_capitals("../Data/state_capitals.csv")
 #TODO: use the dictionary to create a state capital
 #guessing game where the game asks you to name the capital of a state
 #simply iterate through the items in the dictionary
-#use a while True: so you can quiz someone until they want to quit
 
-#Bonus: quiz in a random order
+
+#pick a random key value to get a state and ask user for capital
+#write code to check if answer is correct
+#output whether they are right or what the correct answer is
+
+
+#Bonus: use a while True: so you can quiz someone until they want to quit
+
 
 #Bonus: randomly decide whether to ask for the capital of a state or the
 #to ask for the State given a capital
