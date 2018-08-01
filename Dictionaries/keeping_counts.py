5e5ade4549ed888c6126067c081d7e690d44a4b4
dsbrown1331 <dsbrown1331@gmail.com>
2018-08-01 15:47:01 -0500

    counting

--- Dictionaries/keeping_counts.py
+++ Dictionaries/keeping_counts.py
@@ -1,9 +1,10 @@
 #simple code that counts the frequencies of characters
-word = 'brontosaurus'
+word = 'bbbbaaa'
 d = dict()
 for c in word:
-    if c not in d:
-        d[c] = 1
-    else:
-        d[c] = d[c] + 1
+    #if c not in d:
+    #    d[c] = 1
+    #else:
+    #    d[c] = d[c] + 1
+    d[c] = d.get(c, 0) + 1
 print(d)
---
+++ Dictionaries/text_summarization.py
@@ -0,0 +1,14 @@
+#TODO replace the pass with code that
+#reads a file and counts word frequencies
+#in a dictionary
+#return dictionary of word frequencies
+def count_word_freqs(filename):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    filename = #TODO: filename with path here
+    #TODO: call count_word_freqs and print the result
+    #TODO: print out the most common word in the file
