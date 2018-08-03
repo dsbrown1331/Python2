d38c60a21e9972f47c84dd9c3815e31b3b9defdb
dsbrown1331 <dsbrown1331@gmail.com>
2018-08-03 16:05:10 -0500

    student gpa

--- Classes/dungeon_game.py
+++ Classes/dungeon_game.py
@@ -9,9 +9,10 @@ class Engine(object):
         self.scene_map = scene_map
     def play(self):
         current_scene = self.scene_map.opening_scene()
-        print("\n--------")
-        next_scene_name = current_scene.enter()
-        current_scene = self.scene_map.next_scene(next_scene_name)
+        while True:
+            print("\n--------")
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
 
 class Death(Scene):
     quips = ["You died. Better luck next time.",
@@ -22,49 +23,75 @@ class Death(Scene):
         print(random.choice(self.quips))
         exit()
 
+class OpeningScene(Scene):
+    def enter(self):
+        print("You stand at the entrance to a cave.")
+        print("Wondering what is inside, you enter.")
+        return "cave_enterance"
+
 class CaveEntrance(Scene):
     def enter(self):
-        print("You stand at the entrance to a cave.)
-        print("Wondering what is inside, you enter.")
         print("You see two paths inside the cave.")
-        action = input("Do you go right or left?")
+        action = input("Do you go right or left? ")
         if "right" in action.lower():
             print("You choose to go down the right tunnel")
-            return "right_tunnel"
+            input("Enter to continue")
+            return "treasure_trove"
         elif "left" in action.lower():
             print("You choose to go down the left tunnel")
-            return "left_tunnel"
+            input("Enter to continue")
+            return "dragon_lair"
         else:
             print("Action is not allowed")
-            return  cave_entrance
+            input("Enter to continue")
+            return  'cave_entrance'
 
-class LeftTunnel(Scene):
+class DragonLair(Scene):
     def enter(self):
-        pass
+        print("You come to a large pit.")
+        print("You look down into the pit and feel a strange source of heat.")
+        print("A dragon suddenly appears out of the pit")
 
-class RightTunnel(Scene):
-    def enter(self):
-        pass
+        action = input("Do you run or fight? ")
+        if "run" in action:
+            print("You run back the way you came.")
+            input("Enter to continue")
+            return 'cave_enterance'
+        elif "fight" in action:
+            print("You ball up your fist and punch the dragon square in the nose")
+            print("The dragon opens his mouth incinerates your flesh.")
+            input("Enter to continue")
+            return "death"
+        else:
+            print("Not a valid action")
+            input("Enter to continue")
+            return "dragon_lair"
 
-class Treasure(Scene):
+
+class TreasureTrove(Scene):
     def enter(self):
         print("You find a treasure chest full of gold!")
         print("You return home happy, and very very rich.")
-        return "end_game"
+        exit()
 
 class Map(object):
     scenes = {
-            'cave_enterance' : CaveEntrance()
-            'left_tunnel' : LeftTunnel()
-            'right_tunnel' : RightTunnel()
+            'opening_scene' : OpeningScene(),
+            'cave_enterance' : CaveEntrance(),
+            'dragon_lair' : DragonLair(),
+            'treasure_trove' : TreasureTrove(),
+            'death' : Death()
+            }
 
-    }
+
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
     def next_scene(self, scene_name):
-        pass
+        return self.scenes.get(scene_name)
     def opening_scene(self):
-        pass
-a_map = Map('central_corridor')
-a_game = Engine(a_map)
-a_game.play()
+        return self.next_scene(self.start_scene)
+
+if __name__ == "__main__":
+    a_map = Map('opening_scene')
+    a_game = Engine(a_map)
+    a_game.play()
--- Classes/inheritance.py
+++ Classes/inheritance.py
@@ -33,6 +33,51 @@ class Teacher(Member):
         self.courses_teaching.append(class_name)
 
 
+class Student(Member):
+
+    def __init__(self, name, email, student_id):
+        super().__init__(name, email)
+        self.student_id = student_id
+        self.classes_enrolled = list()
+        self.transcript = dict() #maps from classname to letter grade
+
+    def __str__(self):
+        student_str = super().__str__()
+        student_str += "\n{}".format(self.student_id)
+        student_str += "\nClasses:\n"
+        for c in self.classes_enrolled:
+            student_str += "\t{}\n".format(c)
+        student_str += "\nTranscript:"
+        for k,v in self.transcript.items():
+            student_str += "\n\t{} : {}".format(k,v)
+        student_str += "\nGPA = {}".format(self.compute_gpa())
+        return student_str
+
+    def add_class(self, class_name):
+        self.classes_enrolled.append(class_name)
+
+    def add_class_grade(self, class_name, letter_grade):
+        self.transcript[class_name] = letter_grade
+
+    def letter_to_number(self, letter):
+        if letter == "A":
+            return 4.0
+        elif letter == "B":
+            return 3.0
+        elif letter == "C":
+            return 2.0
+        elif letter == "D":
+            return 1.0
+        else:
+            return 0.0
+
+    def compute_gpa(self):
+        gpa_sum = 0
+        for c, g in self.transcript.items():
+            gpa_sum += self.letter_to_number(g)
+        return gpa_sum / len(self.transcript)
+
+
 
 
 #code to test classes
@@ -44,6 +89,13 @@ def main():
     daniel.add_class("Math")
     daniel.add_class("Sleeping 101")
     print(daniel)
+    print("-------")
+    bill = Student("Bill Bunny", "bbunny@bunny.com", 1345)
+    bill.add_class("Chemistry")
+    bill.add_class("Biology")
+    bill.add_class_grade("English", "B")
+    bill.add_class_grade("Mermaid 101", "A")
+    print(bill)
 
 if __name__=="__main__":
     main()
