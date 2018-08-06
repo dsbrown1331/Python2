e141dbeb42d3141c45a93f607bab3601298dd4a3
dsbrown1331 <dsbrown1331@gmail.com>
2018-08-06 15:58:44 -0500

    vacations.ml

---
+++ Data/vacations.ml
@@ -0,0 +1,15 @@
+#{adj}, adjective
+#{nn}, noun
+#{plnn}, plural noun
+#{vbing}, verb ending in 'ing'
+#{bodypart}, part of the body
+#{number}, number
+#------------------------
+A vacation is when you take a trip some {adj} place with your {adj} family.
+Usually you go to some place this is near a/an {nn} or up on a/an {nn}.
+A good vacation place is one where you can ride {plnn} or play {game} or go hunting for {plnn}.
+I like to spend my time {vbing} or {vbing}.
+When parents go on a vacation, they spend their time eating three {plnn} a day, and fathers golf, and mothers sit around {vbing}.
+Last summer, my little brother fell in a/an {nn} and got poison {plant} all over his {bodypart}.
+My family is going to go to {place}, and I will practice {vbing}.
+Parents need vacations more than kids because parents are always very {adj} and because they have to work {number} hours every day all year making enough {plnn} to pay for the vacation.
