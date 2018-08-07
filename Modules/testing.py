7f079a38dc88de316745875d582b08408ef239cb
dsbrown1331 <dsbrown1331@gmail.com>
2018-08-07 16:02:00 -0500

    modules

--- Modules/testing.py
+++ Modules/testing.py
@@ -1,11 +1,12 @@
 import sys
-#sys.path.append("/home/dsbrown/Documents/ACES/github/Python2/")
-#print(sys.path)
-from package_test import module_test
+sys.path.append("/home/dsbrown/Documents/ACES/github/Python2/")
+from Dictionaries import text_summarization as ts
 
-added = module_test.my_add(3,5)
-print(added)
-multiplied = module_test.my_mult(5,6)
-print(multiplied)
-divided = module_test.my_divide(5,2)
-print(divided)
+filename = "../Data/alice_in_wonderland.txt"
+stopwords = "../Data/stop_words.txt"
+word_cnts = ts.count_word_freqs(filename, stopwords)
+print(word_cnts)
+print('the', word_cnts.get('the',0))
+print('alice', word_cnts.get('alice',0))
+print('liberty', word_cnts.get('liberty',0))
+print("most common word", ts.most_freq_word(word_cnts))
